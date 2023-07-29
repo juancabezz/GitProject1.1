ed6038c87807bf97da7c91a9990e52f45266804d
juancabeza <juancabeza@localhost>
2023-07-29 11:05:43 -0400

    Initial Commit

---
+++ src/day2/DemoDay2.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class DemoDay2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Test");
+	}
+
+	// add method
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	// subtract
+	public static void substract(int a, int b) {
+		System.out.println(a - b);
+	}
+
+}
