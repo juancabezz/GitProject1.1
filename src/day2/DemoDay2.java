8aa205f28c84376fdbe413cee1374e51d53cf38a
juancabeza <juancabeza@localhost>
2023-07-29 13:14:46 -0400

    Added "Day 5" Package

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -3,7 +3,7 @@ package day2;
 public class DemoDay2 {
 
 	public static void main(String[] args) {
-
+		System.out.println("Juan Cabeza...");
 		System.out.println("Test");
 	}
 
---
+++ src/day5/DemoDay5.java
@@ -0,0 +1,10 @@
+package day5;
+
+public class DemoDay5 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Today's weather is 95F in Tampa, Florida...");
+	}
+
+}
